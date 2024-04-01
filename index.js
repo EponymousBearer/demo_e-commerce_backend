1bc3a02a27405faef73d602f6787cdf6e12d6b3a
Muhammad Adnan <115319216+EponymousBearer@users.noreply.github.com>
2024-04-01 11:52:30 +0500

    Update index.js env

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.use("/auth", userRouter);
 app.use("/product", productRouter);
 
 ////////////////////////////////////////////////////////////////////////////
-mongoose.connect(process.env.DB_CONNECTION)
+mongoose.connect("mongodb+srv://adnan:adnannasir@cluster0.o7jt12n.mongodb.net/ecommerce?retryWrites=true&w=majority")
 .then(() => { console.log('Connected Succesfully.') }).catch((err) => console.log('no connection ', err))
 const server = app.listen(PORT, () => console.log("Listening on port ", PORT));

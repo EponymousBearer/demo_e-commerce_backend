84eef9419ba144dd80e2669e5b665ad1029b921e
EponymousBearer <adnasir607@gmail.com>
2024-04-01 11:12:06 +0500

    index.js updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import productRouter from './Routes/product.js'
 import userRouter from './Routes/user.js'
 import session from "express-session";
 
-const PORT = process.env.PORT || 5004;
+const PORT = process.env.PORT || 5005;
 dotenv.config();
 const app = express(); 
 app.use(express.json({ limit: "30mb", extended: true }));
